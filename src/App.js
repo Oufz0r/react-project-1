7a43764795c99981fd8693d0392beceba5b0d1cf
Damien PERNIN <daz.oufzor@gmail.com>
2023-12-18 17:34:57 +0100

    second commit

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header';
 import Toper from './components/Toper';
+import Loading from './components/Loading';
 import Home from "./pages/Home";
 import Apropos from "./pages/Apropos";
 
@@ -11,12 +12,14 @@ function App() {
   return (
     <>
       <div className="bodyContainer">
-        <Toper mail="contact@prettyprint.fdr" phone="+336 000 000 00" horaires="Lundi - Vendredi / 9h - 17h" facebook="/" instagram="/" tiktok="/" />
+        <Toper mail="contact@prettyprint.fr" phone="+336 000 000 00" horaires="Lundi - Vendredi / 9h - 17h" facebook="/" instagram="/" tiktok="/" />
         <Header />
         <Routes>
           {/* <Route path="/location/:locationId" element={<Location />} /> */}
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<Apropos />} />
+          <Route path="/products" element={<Loading />} />
+          <Route path="/contact" element={<Loading />} />
           {/* <Route path="*" element={<Lost />} /> */}
         </Routes>
       </div>
